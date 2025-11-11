4d32d5eb387d3830b2578f5ee4f46587be412217
Szobek Ádám Márk <szobek.adam@gmail.com>
2025-11-11 20:26:22 +0100

    optional optimized build

--- src/morb.bat
+++ src/morb.bat
@@ -1,69 +1,77 @@
-cls
+setlocal
+
+set BARE_COMMAND=..\build_tools\nasm.exe
+set OPTIMIZATION_FLAG=-O0
+
+if "%~1" == "harder" (set OPTIMIZATION_FLAG=-Ox)
+
+set COMMAND=%BARE_COMMAND% %OPTIMIZATION_FLAG%
+
 mkdir build
-..\build_tools\nasm.exe -fobj main.asm -o build/main.o
-..\build_tools\nasm.exe -fobj window/window.asm -o build/window.o
-..\build_tools\nasm.exe -fobj game_loop/game_states.asm -o build/game_states.o
-..\build_tools\nasm.exe -fobj game_loop/settings_loop.asm -o build/settings_loop.o
-..\build_tools\nasm.exe -fobj game_loop/menu_loop.asm -o build/menu_loop.o
-..\build_tools\nasm.exe -fobj game_loop/game_loop.asm -o build/game_loop.o
-..\build_tools\nasm.exe -fobj inventory/hand.asm -o build/hand.o
-..\build_tools\nasm.exe -fobj sun/sun.asm -o build/sun.o
-..\build_tools\nasm.exe -fobj sun/sky.asm -o build/sky.o
-..\build_tools\nasm.exe -fobj world/chunk_manager4d.asm -o build/chunk_manager4d.o
-..\build_tools\nasm.exe -fobj world/chunk4d.asm -o build/chunk4d.o
-..\build_tools\nasm.exe -fobj world/block.asm -o build/block.o
-..\build_tools\nasm.exe -fobj camera/camera.asm -o build/camera.o
-..\build_tools\nasm.exe -fobj audio/sigmaudio_conversion.asm -o build/sigmaudio_conversion.o
-..\build_tools\nasm.exe -fobj audio/sigmaudio.asm -o build/sigmaudio.o
-..\build_tools\nasm.exe -fobj audio/audio.asm -o build/audio.o
-..\build_tools\nasm.exe -fobj input/input.asm -o build/input.o
-..\build_tools\nasm.exe -fobj glfw/glfw.asm -o build/glfw.o
-..\build_tools\nasm.exe -fobj opengl/opengl.asm -o build/opengl.o
-..\build_tools\nasm.exe -fobj shader/shader.asm -o build/shader.o
-..\build_tools\nasm.exe -fobj ui/ui_slider.asm -o build/ui_slider.o
-..\build_tools\nasm.exe -fobj ui/ui_button.asm -o build/ui_button.o
-..\build_tools\nasm.exe -fobj ui/ui_text.asm -o build/ui_text.o
-..\build_tools\nasm.exe -fobj ui/ui_image.asm -o build/ui_image.o
-..\build_tools\nasm.exe -fobj ui/ui_canvas.asm -o build/ui_canvas.o
-..\build_tools\nasm.exe -fobj ui/ui_element.asm -o build/ui_element.o
-..\build_tools\nasm.exe -fobj physics/physics4D.asm -o build/physics4D.o
-..\build_tools\nasm.exe -fobj physics/collider_group_4D.asm -o build/collider_group_4D.o
-..\build_tools\nasm.exe -fobj physics/aabb4D.asm -o build/aabb4D.o
-..\build_tools\nasm.exe -fobj debug/memory_usage_diagram.asm -o build/memory_usage_diagram.o
-..\build_tools\nasm.exe -fobj utils/animation_curve.asm -o build/animation_curve.o
-..\build_tools\nasm.exe -fobj utils/perlin.asm -o build/perlin.o
-..\build_tools\nasm.exe -fobj utils/math.asm -o build/math.o
-..\build_tools\nasm.exe -fobj utils/qsort.asm -o build/qsort.o
-..\build_tools\nasm.exe -fobj utils/thread_safe_vector.asm -o build/thread_safe_vector.o
-..\build_tools\nasm.exe -fobj utils/thread_safe_queue.asm -o build/thread_safe_queue.o
-..\build_tools\nasm.exe -fobj utils/thread_safe_value.asm -o build/thread_safe_value.o
-..\build_tools\nasm.exe -fobj utils/multithreading.asm -o build/multithreading.o
-..\build_tools\nasm.exe -fobj utils/queue.asm -o build/queue.o
-..\build_tools\nasm.exe -fobj utils/hashmap.asm -o build/hashmap.o
-..\build_tools\nasm.exe -fobj utils/vector.asm -o build/vector.o
-..\build_tools\nasm.exe -fobj utils/string.asm -o build/string.o
-..\build_tools\nasm.exe -fobj utils/console.asm -o build/console.o
-..\build_tools\nasm.exe -fobj utils/meminfo.asm -o build/meminfo.o
-..\build_tools\nasm.exe -fobj utils/memory.asm -o build/memory.o
-..\build_tools\nasm.exe -fobj utils/file.asm -o build/file.o
-..\build_tools\nasm.exe -fobj utils/ctype.asm -o build/ctype.o
-..\build_tools\nasm.exe -fobj utils/cvt.asm -o build/cvt.o
-..\build_tools\nasm.exe -fobj glm3/vec3.asm -o build/vec3.o
-..\build_tools\nasm.exe -fobj glm3/vec4.asm -o build/vec4.o
-..\build_tools\nasm.exe -fobj glm3/mat3.asm -o build/mat3.o
-..\build_tools\nasm.exe -fobj glm3/mat4.asm -o build/mat4.o
-..\build_tools\nasm.exe -fobj settings/settings.asm -o build/settings.o
-..\build_tools\nasm.exe -fobj player/player.asm -o build/player.o
-..\build_tools\nasm.exe -fobj hypershapes/hyperplane.asm -o build/hyperplane.o
-..\build_tools\nasm.exe -fobj renderable/hypercube_renderable.asm -o build/hypercube_renderable.o
-..\build_tools\nasm.exe -fobj renderable/geometry_importer.asm -o build/geometry_importer.o
-..\build_tools\nasm.exe -fobj renderable/renderable.asm -o build/renderable.o
-..\build_tools\nasm.exe -fobj renderer/text/font.asm -o build/font.o
-..\build_tools\nasm.exe -fobj renderer/text/text_renderer.asm -o build/text_renderer.o
-..\build_tools\nasm.exe -fobj renderer/post_processing/post_processing.asm -o build/post_processing.o
-..\build_tools\nasm.exe -fobj renderer/framebuffer/framebuffer.asm -o build/framebuffer.o
-..\build_tools\nasm.exe -fobj image/image.asm -o build/image.o
-..\build_tools\nasm.exe -fobj renderer/texture/texture_handler.asm -o build/texture_handler.o
+%COMMAND% -fobj main.asm -o build/main.o
+%COMMAND% -fobj window/window.asm -o build/window.o
+%COMMAND% -fobj game_loop/game_states.asm -o build/game_states.o
+%COMMAND% -fobj game_loop/settings_loop.asm -o build/settings_loop.o
+%COMMAND% -fobj game_loop/menu_loop.asm -o build/menu_loop.o
+%COMMAND% -fobj game_loop/game_loop.asm -o build/game_loop.o
+%COMMAND% -fobj inventory/hand.asm -o build/hand.o
+%COMMAND% -fobj sun/sun.asm -o build/sun.o
+%COMMAND% -fobj sun/sky.asm -o build/sky.o
+%COMMAND% -fobj world/chunk_manager4d.asm -o build/chunk_manager4d.o
+%COMMAND% -fobj world/chunk4d.asm -o build/chunk4d.o
+%COMMAND% -fobj world/block.asm -o build/block.o
+%COMMAND% -fobj camera/camera.asm -o build/camera.o
+%COMMAND% -fobj audio/sigmaudio_conversion.asm -o build/sigmaudio_conversion.o
+%COMMAND% -fobj audio/sigmaudio.asm -o build/sigmaudio.o
+%COMMAND% -fobj audio/audio.asm -o build/audio.o
+%COMMAND% -fobj input/input.asm -o build/input.o
+%COMMAND% -fobj glfw/glfw.asm -o build/glfw.o
+%COMMAND% -fobj opengl/opengl.asm -o build/opengl.o
+%COMMAND% -fobj shader/shader.asm -o build/shader.o
+%COMMAND% -fobj ui/ui_slider.asm -o build/ui_slider.o
+%COMMAND% -fobj ui/ui_button.asm -o build/ui_button.o
+%COMMAND% -fobj ui/ui_text.asm -o build/ui_text.o
+%COMMAND% -fobj ui/ui_image.asm -o build/ui_image.o
+%COMMAND% -fobj ui/ui_canvas.asm -o build/ui_canvas.o
+%COMMAND% -fobj ui/ui_element.asm -o build/ui_element.o
+%COMMAND% -fobj physics/physics4D.asm -o build/physics4D.o
+%COMMAND% -fobj physics/collider_group_4D.asm -o build/collider_group_4D.o
+%COMMAND% -fobj physics/aabb4D.asm -o build/aabb4D.o
+%COMMAND% -fobj debug/memory_usage_diagram.asm -o build/memory_usage_diagram.o
+%COMMAND% -fobj utils/animation_curve.asm -o build/animation_curve.o
+%COMMAND% -fobj utils/perlin.asm -o build/perlin.o
+%COMMAND% -fobj utils/math.asm -o build/math.o
+%COMMAND% -fobj utils/qsort.asm -o build/qsort.o
+%COMMAND% -fobj utils/thread_safe_vector.asm -o build/thread_safe_vector.o
+%COMMAND% -fobj utils/thread_safe_queue.asm -o build/thread_safe_queue.o
+%COMMAND% -fobj utils/thread_safe_value.asm -o build/thread_safe_value.o
+%COMMAND% -fobj utils/multithreading.asm -o build/multithreading.o
+%COMMAND% -fobj utils/queue.asm -o build/queue.o
+%COMMAND% -fobj utils/hashmap.asm -o build/hashmap.o
+%COMMAND% -fobj utils/vector.asm -o build/vector.o
+%COMMAND% -fobj utils/string.asm -o build/string.o
+%COMMAND% -fobj utils/console.asm -o build/console.o
+%COMMAND% -fobj utils/meminfo.asm -o build/meminfo.o
+%COMMAND% -fobj utils/memory.asm -o build/memory.o
+%COMMAND% -fobj utils/file.asm -o build/file.o
+%COMMAND% -fobj utils/ctype.asm -o build/ctype.o
+%COMMAND% -fobj utils/cvt.asm -o build/cvt.o
+%COMMAND% -fobj glm3/vec3.asm -o build/vec3.o
+%COMMAND% -fobj glm3/vec4.asm -o build/vec4.o
+%COMMAND% -fobj glm3/mat3.asm -o build/mat3.o
+%COMMAND% -fobj glm3/mat4.asm -o build/mat4.o
+%COMMAND% -fobj settings/settings.asm -o build/settings.o
+%COMMAND% -fobj player/player.asm -o build/player.o
+%COMMAND% -fobj hypershapes/hyperplane.asm -o build/hyperplane.o
+%COMMAND% -fobj renderable/hypercube_renderable.asm -o build/hypercube_renderable.o
+%COMMAND% -fobj renderable/geometry_importer.asm -o build/geometry_importer.o
+%COMMAND% -fobj renderable/renderable.asm -o build/renderable.o
+%COMMAND% -fobj renderer/text/font.asm -o build/font.o
+%COMMAND% -fobj renderer/text/text_renderer.asm -o build/text_renderer.o
+%COMMAND% -fobj renderer/post_processing/post_processing.asm -o build/post_processing.o
+%COMMAND% -fobj renderer/framebuffer/framebuffer.asm -o build/framebuffer.o
+%COMMAND% -fobj image/image.asm -o build/image.o
+%COMMAND% -fobj renderer/texture/texture_handler.asm -o build/texture_handler.o
 ..\build_tools\alink.exe -subsys console -oPE ^
 build/main.o ^
 build/window.o ^
@@ -137,5 +145,5 @@ rmdir build /s /q
 cd ..
 cd game_files
 test.exe
-cd ..
-cd src
+
+endlocal
